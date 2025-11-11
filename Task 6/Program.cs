76443f8c0493958a146e7e810a733ca28c459170
Hentix <iu3071pc@gmail.com>
2025-11-11 18:48:55 +0545

    Task 5 & 6

--- Task 6/Program.cs
+++ Task 6/Program.cs
@@ -1,11 +1,11 @@
-﻿namespace Task5;
+﻿namespace Task6;
+
 class Program
 {
     public static void Main(string[] args)
     {
         Methods obj = new Methods();
-        obj.SumNumber();
-        obj.SumForEach();
-        obj.PrintNumbers();
+        obj.TryCatch();
+        obj.PasswordStrength();
     }
 }
